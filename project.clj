9764832dad4edac84e79ebc0f738206984afc762
jiangplus <jiang@jiang.com>
2019-01-01 20:42:33 +0800

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject claire "0.1.0-SNAPSHOT"
+  :description "a clojure app"
+  :url "https://github.com/jiangplus/claire"
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot claire.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/claire/core_test.clj
@@ -0,0 +1,7 @@
+(ns claire.core-test
+  (:require [clojure.test :refer :all]
+            [claire.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
