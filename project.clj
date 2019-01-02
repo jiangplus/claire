1df34750c7f6cc9e613883f9cadef19cebff1ae2
jiangplus <jiang@jiang.com>
2019-01-02 23:52:37 +0800

    add clj-http

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "a clojure app"
   :url "https://github.com/jiangplus/claire"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [clj-http "3.9.1"]]
   :main ^:skip-aot claire.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
